eb15833aed3738fee6132808b27a606a5c1f1d4a
Marcelo Tedesco Giglio <marcelotedesco@yahoo.com.br>
2021-02-08 11:59:57 -0300

    WebSecurityConfig

--- src/main/java/com/mms/security/WebSecurityConfig.java
+++ src/main/java/com/mms/security/WebSecurityConfig.java
@@ -59,8 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests()
 				.antMatchers("/api/auth/**").permitAll()
-				.antMatchers("/api/mms-sender/**").permitAll()
-//				.antMatchers("/api/mms-sender/template/**").permitAll()
+//				.antMatchers("/api/mms-sender/**").permitAll()
 				.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
