dd7c92e21463557f77b25605e217f0c54ca94a0a
rodrigo.lima <rodrigo.lima@FTFH3Z2.internal.upstreamsystems.com>
2021-03-11 12:59:32 -0300

    Removed unused imports

--- src/main/java/com/mms/security/controller/AuthController.java
+++ src/main/java/com/mms/security/controller/AuthController.java
@@ -1,38 +1,25 @@
 package com.mms.security.controller;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import com.mms.security.registration.OnRegistrationCompleteEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mms.model.ERole;
-import com.mms.model.Role;
-import com.mms.model.User;
-import com.mms.repository.RoleRepository;
-import com.mms.repository.UserRepository;
 import com.mms.security.jwt.JwtUtils;
 import com.mms.security.payload.request.LoginRequest;
-import com.mms.security.payload.request.SignupRequest;
 import com.mms.security.payload.response.JwtResponse;
-import com.mms.security.payload.response.MessageResponse;
 import com.mms.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
